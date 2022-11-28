c57e0347c4a6ecd33313041b2d1a75876fde8f31
Joe-Hsn <youssef.hassan.mahmoud@gmail.com>
2022-11-28 11:11:24 +0200

    slider done

---
+++ script.js
@@ -0,0 +1,18 @@
+// swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  speed: 400,
+  autoplay: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+});
+// AOS
+AOS.init({
+  duration: 800,
+  easing: "ease-in-out",
+});
